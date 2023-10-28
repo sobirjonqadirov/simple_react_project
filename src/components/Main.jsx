bc4cd9243c1a84d95b093d8d7d6b029a33712905
sobirjonqadirov <sobirjon.dev@gmail.com>
2023-10-28 18:32:49 +0500

    final commit

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,16 @@
 export default function Main() {
     return (
-        <h1>Main goes here</h1>
+        <div className="main">
+            <h1 className="main--title">Fun facts about React</h1>
+            <div className="main--content">
+                <ul className="main--list">
+                    <li><p>Was first released in 2013</p></li>
+                    <li><p>Was originally created by Jordan Walke</p></li>
+                    <li><p>Has well over 100K stars on GitHub</p></li>
+                    <li><p>Is maintained by Facebook</p></li>
+                    <li><p>Powers thousands of enterprise apps, including mobile apps</p></li>
+                </ul>
+            </div>
+        </div>
     )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,12 +3,12 @@ import img from "../images/react-icon-small.png"
 export default function Navbar() {
     return (
         <nav>
-            <div className="logo">
+            <div className="nav--logo">
                 <img src={img} alt="react-logo" />
                 <p>ReactFacts</p>
             </div>
-            <div className="title">
-                <p>React Course - Project 1</p>
+            <div className="nav--title">
+                <h4>React Course - Project 1</h4>
             </div>
         </nav>
     )
